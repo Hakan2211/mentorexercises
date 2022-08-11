1eab089cfa3fff7cd0ec26315df7c1a888c031b8
Hakan Bilgic <hbilgic1992@gmail.com>
2022-08-11 09:44:19 +0200

    initial commit

---
+++ github_search/src/main.jsx
@@ -0,0 +1,8 @@
+import react from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+App;
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(<App />);
